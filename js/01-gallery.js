c46b461fd291f549f7f9790a1d200a65d63ed08b
lilyzeta <128751126+tfullest@users.noreply.github.com>
2023-06-18 14:50:51 -0400

    adjust

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(
+      (item) =>
+        `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+}
